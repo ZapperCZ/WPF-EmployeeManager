8cd8dae378dd084000719e58c0a2a6923d238859
Jan Frídl <fridl.jan.2018@skola.ssps.cz>
2021-03-26 20:53:26 +0100

    Frontend: Updated UI Backend: Added new methods for buttons and added most of methods needed for later

--- WPF-EmployeeManager/WPF-EmployeeManager/MainWindow.xaml.cs
+++ WPF-EmployeeManager/WPF-EmployeeManager/MainWindow.xaml.cs
@@ -20,26 +20,73 @@ namespace WPF_EmployeeManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Label> ErrorLabels = new List<Label>();
+        Employee[] Employees = new Employee[5];
         public MainWindow()
         {
             InitializeComponent();
+            foreach(Control control in RootGrid.Children)
+            {
+                if (control.Name.ToLower().Contains("error"))
+                {
+                    ErrorLabels.Add((Label)control);
+                }
+            }
         }
         public void AddNewEmployee(object sender, RoutedEventArgs eventArgs)
         {
-
+            //Employee NewEmployee = new Employee();
+            UpdateOutput();
+        }
+        public void RemoveEmployee(object sender, RoutedEventArgs eventArgs)
+        {
+            UpdateOutput();
         }
         public void LostFocus(object sender, RoutedEventArgs eventArgs)
         {
-            TextBox SenderTB = (TextBox) sender;
+            TextBox SenderTB = new TextBox();
+            SenderTB = (TextBox)sender;
             Label ErrorLabel = (Label)RootGrid.FindName(SenderTB.Name + "Error");
             if (SenderTB.Text == "")
             {
                 ErrorLabel.Visibility = Visibility.Visible;
             }
-            else if(SenderTB.Text!="" && SenderTB.IsVisible)
+            else if (SenderTB.Text != "" && ErrorLabel.IsVisible)
             {
                 ErrorLabel.Visibility = Visibility.Hidden;
             }
+            ButtonAddEmployee.IsEnabled = IsEverythingFilledOut();
+        }
+
+        void OnDropDownClosed(object sender, EventArgs e)
+        {
+            ComboBox SenderCB = (ComboBox)sender;
+            Label ErrorLabel = (Label)RootGrid.FindName(SenderCB.Name + "Error");
+            if (SenderCB.Text == "")
+            {
+                ErrorLabel.Visibility = Visibility.Visible;
+            }
+            else if (SenderCB.Text != "" && ErrorLabel.IsVisible)
+            {
+                ErrorLabel.Visibility = Visibility.Hidden;
+            }
+            ButtonAddEmployee.IsEnabled = IsEverythingFilledOut();
+        }
+        bool IsEverythingFilledOut()
+        {
+            bool result = true;
+            foreach(Label ErrorLabel in ErrorLabels)
+            {
+                if (ErrorLabel.IsVisible)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+        void UpdateOutput()
+        {
+
         }
     }
 }
